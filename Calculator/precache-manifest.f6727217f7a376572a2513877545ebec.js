aff854a4d62dc251da7a809293f167d21bdaa15e
Dmitrij Mosejko <znaten@gmail.com>
2019-08-07 19:58:58 +0300

    added react project and update main index.html

---
+++ Calculator/precache-manifest.f6727217f7a376572a2513877545ebec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8550bff74202fd08e985074b524177bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cc293bf6427854cea04",
+    "url": "/static/css/main.a52505d5.chunk.css"
+  },
+  {
+    "revision": "ce3784147a5068090e10",
+    "url": "/static/js/2.206fbce5.chunk.js"
+  },
+  {
+    "revision": "4cc293bf6427854cea04",
+    "url": "/static/js/main.f9599dd0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5bf7a60b15a920d37a72438889ad40dd",
+    "url": "/static/media/mockup.5bf7a60b.png"
+  }
+]);
